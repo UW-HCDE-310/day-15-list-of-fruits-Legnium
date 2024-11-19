4a86b1685164bf0b679fbca9c5eb88184a3505e3
Legnium <angelt22@uw.edu>
2024-11-19 10:51:00 -0800

    Should avoid the keys.py

--- app.py
+++ app.py
@@ -8,4 +8,10 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+    appended_fruit = []
+    for fruit in fruits:
+        # Check if the name starts with 'o' and quantity is less than 3
+        if fruit['name'][0] == 'o' and fruit['quantity'] < 3:
+            appended_fruit.append(fruit)
+
+    return render_template("index.html", fruits=appended_fruit)
